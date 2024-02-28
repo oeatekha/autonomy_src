6986de41fb7f9fcd70c2aff59dfbfc290a2553a9
HarrisonGuan <harrisonguan1213@gmail.com>
2024-02-27 17:50:44 -0800

    Added ROS package

--- build/move_arms/CMakeFiles/move_arms_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/move_arms_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/move_arms_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/move_without_obstacle/CMakeFiles/move_without_obstacle_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/move_without_obstacle_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/move_without_obstacle_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
